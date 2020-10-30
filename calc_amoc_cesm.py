98db78d144c19b2f757ea0326f113da44c1add03
Glenn <glenn.y.liu@gmail.com>
2020-10-30 18:36:39 -0400

    Added seasonal cycle removal to AMOC preprocessing
    Added the option to remove the monthly climatological mean (seasonal cycle) for each ensemble member individually for AMOC index calculation. Updated the data accordingly.

--- calc_amoc_cesm.py
+++ calc_amoc_cesm.py
@@ -8,7 +8,10 @@ General Procedure:
     1 - Load Data
     2 - Cut to region 
     3 - sum selected components
-    3 - calculate index by taking depth and latitude maximum
+    4 - Remove seasonal cycle/monthly climatology
+    5 - Take maximum from selected region (depth and latitude)
+    6 - Detrend
+    7 - Save the data
     
     
 
@@ -45,7 +48,11 @@ latN = 30
 # Detrending Options
 # For deg = 0, Detrend by removing the ensemble average 
 # For deg > 0, Specify degree of polynomial for detrending 
-deg = 1
+deg = 0
+
+# Remove Monthly Anomalies
+canom = 0 # Set to 1 to remove climatologial mean for each ensemble
+manom = 1 # Set to 1 to remove monthly anomalies before processing
         
 # Debug mode, makes some plots to check detrending, etc
 debug = 1
@@ -57,8 +64,7 @@ debug = 1
 comps = [0,1,2]
 
 ## User Edits <END> ----
-#%% 1) Load Data and calculate monthly anomalies ----
-
+#%% 1) Load Data 
 # Import module with functions
 sys.path.append(modpath)
 import amvmod as amv
@@ -86,16 +92,51 @@ if len(comps)>1:
 else:
     compname = str(comps[:])
 
+
+#%% 4 -- Remove specified anomalies
+
+# Remove climatological mean
+if canom == 1:
+    clim = np.mean(moc,axis=1,keepdims=True)
+    moc = moc - clim
+    
+    if debug ==1:
+        e = 0
+        
+        fig,axs= plt.subplots(1,2,figsize=(8,2))
+        
+        cblimit = np.nanmax(moc[e,0,:,:])
+        cint = np.linspace(-cblimit,cblimit,11)
+        
+        ax = axs[0]
+        plt.gca().invert_yaxis()
+        pcm0 = ax.contourf(lat,dz,moc[e,0,:,:],levels=cint,cmap='seismic')
+        ax.set_xlabel("Depth (Centimeters)")
+        ax.set_xlabel("Latitude (degN)")
+        ax.set_title("MOC Anomaly")
+        fig.colorbar(pcm0,ax=ax)
+        
+        
+        ax= axs[1]
+        plt.gca().invert_yaxis()
+        pcm = ax.contourf(lat,dz,clim[e,0,:,:])
+        ax.set_xlabel("Depth (Centimeters)")
+        ax.set_xlabel("Latitude (degN)")
+        ax.set_title("MOC Climatology")
+        fig.colorbar(pcm,ax=ax)
+
 # Calculate monthly anomalies
-# moc = moc.tranpose(1,0,2,3) # [time x ens x depth x lat]
-# mocm = moc.reshape(int(np.ceil(nmon/12)),12,nens*nz*nlat) # Separate mon/year, combine lat/depth/ens
-# moca = mocm - mocm.mean(0)[None,:,:] # Calculate monthly anomalies
-# moca = moca.reshape(nmon,nens*nz*nlat) # Recombine mon x year
+if manom == 1:
+    moc  = moc.transpose(1,0,2,3) # --> [time x ens x depth x lat]
+    mocm = moc.reshape(int(np.ceil(nmon/12)),12,nens*nz*nlat) # Separate mon/year, combine lat/depth/ens
+    moca = mocm - mocm.mean(0)[None,:,:] # Calculate monthly anomalies
+    moca = moca.reshape(nmon,nens,nz*nlat) # --> [time x ens x depth*lat]
+    moc  = moca.transpose(1,0,2) # --> [ens x time x depth*lat]
+else:
+    moc = moc.reshape(nens,nmon,nz*nlat)# Combne depth + lat
 
-
-#%% 4 -- Take the maximum from the selected region
-
-amocidx = moc.reshape(nens,nmon,nz*nlat).max(2) # [ensemble x time]
+#%% 5 -- Take the maximum from the selected region
+amocidx = moc.max(2) # [ensemble x time]
 
 if debug == 1:
     e = 0
@@ -105,7 +146,7 @@ if debug == 1:
     ax.set_ylabel("MOC (Sverdrups)")
     ax.set_title("AMOC Index (Undetrended) for Ensemble member %i, Lat: %iN to %iN"%(e+1,latS,latN))
 
-#%% 5 -- Detrend
+#%% 6 -- Detrend
 
 if deg == 0: # Remove ensemble average to detrend
     amocidxdt = amocidx - amocidx.mean(0)[None,:]
@@ -122,7 +163,6 @@ if deg == 0: # Remove ensemble average to detrend
         ax.set_ylabel("MOC (Sverdrups)")
         
         ax.set_title("AMOC Index for Ensemble member %i, Lat: %iN to %iN"%(e+1,latS,latN))
-        
 elif deg > 0:
     x = np.arange(0,nmon)
     amocidxdt,model = amv.detrend_poly(x,amocidx.T,deg) # [time x ensemble]
@@ -142,7 +182,5 @@ elif deg > 0:
   
     amocidxdt = amocidxdt.T #[ensemble x time]
     
-
-
 #%% 5 - Save Output
 np.save("%sCESM1LE_AMOCIndex_%s-%s_region%ito%i_component%s_detrend%i.npy" % (outpath,start[0:4],end[0:4],latS,latN,compname,deg),amocidxdt)
