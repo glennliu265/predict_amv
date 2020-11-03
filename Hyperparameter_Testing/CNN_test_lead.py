17b2febc7e6b4c7902ffe52f5395b98d9ab503e6
Glenn <glenn.y.liu@gmail.com>
2020-11-03 16:46:50 -0500

    Small bug fix and added lead testing
    -Small bug fix in the batch/epoch calculations (removed need to take minimum over axis 2)
    -Added script to experiment with different lead times given a fixed CNN architecture.

--- Hyperparameter_Testing/CNN_test_lead.py
+++ Hyperparameter_Testing/CNN_test_lead.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-CNN_test_hyperparameters_batch_epochs
 
-Testing batchsize and epochs
+"""
+CNN_test_lead_time
+
+testing different lead times
 
 """
 
@@ -117,161 +116,152 @@ if machine == 'local-glenn':
 else:
     outpath = os.getcwd()
 # Data preparation settings
-lead          = 12    # Time ahead (in months) to forecast AMV
-tstep         = 1032  # Total number of months
+leads          = np.arange(0,25,1)    # Time ahead (in months) to forecast AMV
+tstep          = 1032                  # Total number of months
 
 percent_train = 0.8   # Percentage of data to use for training (remaining for testing)
-ens           = 1    # Ensemble members to use
+ens           = 10    # Ensemble members to use
 
 # Select variable
-channels   = 3     # Number of variables to include
-varname    = 'SST+SSS+PSL'
+#channels   = 3     # Number of variables to include
+#varname    = 'SST+SSS+PSL'
 sst_normed = np.load('../CESM_data/CESM_SST_normalized_lat_weighted.npy').astype(np.float32)
 sss_normed = np.load('../CESM_data/CESM_SSS_normalized_lat_weighted.npy').astype(np.float32)
 psl_normed = np.load('../CESM_data/CESM_PSL_normalized_lat_weighted.npy').astype(np.float32)
-invars = [sst_normed,sss_normed,psl_normed]
+#invars = [sst_normed,sss_normed,psl_normed]
 #invars=[psl_normed]
 
 # Model training settings
-#max_epochs    = 15 
-#batch_size    = 32                    # Pairs of predictions
+max_epochs    = 5 
+batch_size    = 32                    # Pairs of predictions
 loss_fn       = nn.MSELoss()          # Loss Function
 opt           = ['Adadelta',0.1,0]  # Name optimizer
 nchannels     = 32                    # Number of out_channels for the first convolution
 filtersize1   = 5# kernel size for first ConvLayer
 poolsize1     = 5# kernel size for first pooling layer
 
-
-
-
-
-# ---------------------
-#%% Load and prep data
-# ---------------------
-
-allstart = time.time()
-
-# Apply lead/lag to data
-y = np.mean(sst_normed[:ens,lead:,:,:],axis=(2,3)).reshape((tstep-lead)*ens,1) # Take area average for SST 
-X = np.transpose(
-    np.array(invars)[:,:ens,0:tstep-lead,:,:].reshape(channels,(tstep-lead)*ens,33,89),
-    (1,0,2,3))
-
-# Print shapes
-print("y is size: %s" % str(y.shape)) # [ data (ensemble*time) x 1 ]
-print("X is size: %s" % str(X.shape)) # [ data (ensemble*time) x variable x lat x lon]
-
-# Split into training and test sets
-X_train = torch.from_numpy( X[0:int(np.floor(percent_train*(tstep-lead)*ens)),:,:,:] )
-y_train = torch.from_numpy( y[0:int(np.floor(percent_train*(tstep-lead)*ens)),:] )
-
-X_val = torch.from_numpy( X[int(np.floor(percent_train*(tstep-lead)*ens)):,:,:,:] )
-y_val = torch.from_numpy( y[int(np.floor(percent_train*(tstep-lead)*ens)):,:] )
-
-# Print shapes
-print("Training set is size: %i" % y_train.shape[0]) 
-print("Validation set is size: %i" %y_val.shape[0])  
-
-
-
-
 # ----------------------------------------
-# %% Manual Hyperparameter Testing: Set-up
+# %% Experiment storage setup
 # ----------------------------------------
-# Setup: Indicate hyperparameter values and set up grid. Preallocate output variables and prepare data
-# Currently, only works with varying eta and wd (need to think of more generalizable code...)
 
-# User edits start here -----------------------
-# Input some hyperparameters and their values, as well as choices for the data
-
-
-pr1name = 'batchsize'
-pr2name = 'epochs'
-
-# Set the hyperparameter grid
-param1 = [16,32,64,128] # Indicate batch sizes here
-param2 = [1,5,10,15]      # Indicate max training epochs here
+nvar = 4 # Combinations of variables to test
+nlead = len(leads)
 
 # Save data
-expname = "%s_%ito%i_%s_%ito%i_nens%i_lead%i_%s" % (pr1name,min(param1),max(param1),pr2name,min(param2),max(param2),ens,lead,varname)
-outname = "hyperparameter_testing_%s.npz" % (expname)
+expname = "nens%i" % (ens)
 
-# Dimensions of target parameters
-np1 = len(param1)
-np2 = len(param2)
-gridsize = np.array([np1,np2]) 
 
 # Preallocate Relevant Variables...
-corr_grid_train = np.zeros(gridsize)
-corr_grid_test  = np.zeros(gridsize)
-train_loss_grid = np.zeros(gridsize)
-test_loss_grid  = np.zeros(gridsize)
+corr_grid_train = np.zeros((nlead))
+corr_grid_test  = np.zeros((nlead))
+train_loss_grid = np.zeros((max_epochs,nlead))
+test_loss_grid  = np.zeros((max_epochs,nlead))
 
 # ----------------------------------------------
 # %% Manual Hyperparameter Testing: Testing Loop
 # ----------------------------------------------
 
-ndat,_,nlat,nlon = X.shape # Get latitude and longitude sizes for dimension calculation
-
- # Calculate dimensions of first FC layer
-firstlineardim = calc_layerdim(nlat,nlon,filtersize1,poolsize1,nchannels)
-
-# Set layer architecture
-layers        = [
-                nn.Conv2d(in_channels=channels, out_channels=nchannels, kernel_size=filtersize1),
-                nn.ReLU(),
-                nn.MaxPool2d(kernel_size=poolsize1),
-                nn.Flatten(),
-                nn.Linear(in_features=firstlineardim,out_features=128),
-                nn.ReLU(),
-                nn.Linear(in_features=128,out_features=64),
-                nn.ReLU(),
-                nn.Dropout(p=0.5),
-                nn.Linear(in_features=64,out_features=1)
-                ]
-
-for i in range(np1): # Loop for Learning Rate
-    batch_size = param1[i]
+channels = 1
+for v in range(nvar): # Loop for each variable
+    start = time.time()
+    if v == 0:
+        varname = 'SST'
+        invars = [sst_normed]
+    elif v == 1:
+        varname = 'SSS'
+        invars = [sss_normed]
+    elif v == 2:
+        varname = 'PSL'
+        invars = [psl_normed]
+    elif v == 3:
+        channels = 3
+        varname = 'ALL'
+        invars = [sst_normed,sss_normed,psl_normed]
     
-    # Put into pytorch DataLoader
-    train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size)
-    val_loader   = DataLoader(TensorDataset(X_val, y_val), batch_size=batch_size)
+    outname = "/leadtime_testing_%s_%s.npz" % (varname,expname)
     
-    for j in range(np2): # Loop for Weight Decay
-        start = time.time()
-        max_epochs = param2[j]
+    for l,lead in enumerate(leads):
+
+        # Apply lead/lag to data
+        y = np.mean(sst_normed[:ens,lead:,:,:],axis=(2,3)).reshape((tstep-lead)*ens,1) # Take area average for SST 
+        X = np.transpose(
+            np.array(invars)[:,:ens,0:tstep-lead,:,:].reshape(channels,(tstep-lead)*ens,33,89),
+            (1,0,2,3))
+        
+        
+        # Split into training and test sets
+        X_train = torch.from_numpy( X[0:int(np.floor(percent_train*(tstep-lead)*ens)),:,:,:] )
+        y_train = torch.from_numpy( y[0:int(np.floor(percent_train*(tstep-lead)*ens)),:] )
+        
+        X_val = torch.from_numpy( X[int(np.floor(percent_train*(tstep-lead)*ens)):,:,:,:] )
+        y_val = torch.from_numpy( y[int(np.floor(percent_train*(tstep-lead)*ens)):,:] )
+        
+        # Put into pytorch DataLoader
+        train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size)
+        val_loader   = DataLoader(TensorDataset(X_val, y_val), batch_size=batch_size)
+            
+        # ---
+
+        ndat,_,nlat,nlon = X.shape # Get latitude and longitude sizes for dimension calculation
+        
+         # Calculate dimensions of first FC layer
+        firstlineardim = calc_layerdim(nlat,nlon,filtersize1,poolsize1,nchannels)
+
+        # Set layer architecture
+        layers        = [
+                        nn.Conv2d(in_channels=channels, out_channels=nchannels, kernel_size=filtersize1),
+                        nn.ReLU(),
+                        nn.MaxPool2d(kernel_size=poolsize1),
+                        nn.Flatten(),
+                        nn.Linear(in_features=firstlineardim,out_features=128),
+                        nn.ReLU(),
+                        nn.Linear(in_features=128,out_features=64),
+                        nn.ReLU(),
+                        nn.Dropout(p=0.5),
+                        nn.Linear(in_features=64,out_features=1)
+                        ]
         
         # Train the model
         model,trainloss,testloss = train_CNN(layers,loss_fn,opt,train_loader,val_loader,max_epochs,verbose=False)
-        
+            
         # Save train/test loss
-        train_loss_grid[i,j] = np.array(trainloss).min().squeeze() # Take minum of each epoch
-        test_loss_grid[i,j]  = np.array(testloss).min().squeeze()
-        
+        train_loss_grid[:,l] = np.array(trainloss).min().squeeze() # Take minum of each epoch
+        test_loss_grid[:,l]  = np.array(testloss).min().squeeze()
+            
         # Evalute the model
         y_pred_val   = model(X_val).detach().numpy()
         y_valdt        = y_val.detach().numpy()
         y_pred_train = model(X_train).detach().numpy()
         y_traindt      = y_train.detach().numpy()
-        
+            
         # Get the correlation (save these)
         traincorr = np.corrcoef( y_pred_train.T[0,:], y_traindt.T[0,:])[0,1]
         testcorr  = np.corrcoef( y_pred_val.T[0,:], y_valdt.T[0,:])[0,1]
         
+        if np.isnan(traincorr) | np.isnan(testcorr):
+            print("Warning, NaN Detected for %s lead %i of %i in %.2fs. Stopping!" % (varname,lead,len(leads)))
+            break
+        
         # Calculate Correlation and RMSE
-        corr_grid_test[i,j]    = np.corrcoef( y_pred_val.T[0,:], y_valdt.T[0,:])[0,1]
-        corr_grid_train[i,j]   = np.corrcoef( y_pred_train.T[0,:], y_traindt.T[0,:])[0,1]
+        corr_grid_test[l]    = np.corrcoef( y_pred_val.T[0,:], y_valdt.T[0,:])[0,1]
+        corr_grid_train[l]   = np.corrcoef( y_pred_train.T[0,:], y_traindt.T[0,:])[0,1]
         
-        print("\nCompleted training for %s %i of %i, %s %i of %i in %.2fs" % (pr1name,i+1,np1,pr2name,j+1,np2,time.time()-start))
+        print("\nCompleted training for %s lead %i of %i in %.2fs" % (varname,lead,len(leads),time.time()-start))
+    # Save Data
+    np.savez(outpath+outname,
+             train_loss_grid,
+             test_loss_grid,
+             corr_grid_test,
+             corr_grid_train
+            )
+    print("Saved data to %s%s. Script ran to completion in %ss"%(outpath,outname,time.time()-start))
+
         
-# Save Data
-np.savez(outpath+outname,
-         train_loss_grid,
-         test_loss_grid,
-         corr_grid_test,
-         corr_grid_train
-        )
-print("Saved data to %s%s. Script ran to completion in %ss"%(outpath,outname,time.time()-allstart))
+    
+    
+
+
+#%%
 
 
 
