498d2eac117bb793727810bc03c89ee65561d181
Glenn <glenn.y.liu@gmail.com>
2022-11-30 13:56:33 -0500

    Add runs rather than leadtimes

--- Models/NN_test_lead_ann_ImageNet_classification_singlevar.py
+++ Models/NN_test_lead_ann_ImageNet_classification_singlevar.py
@@ -41,7 +41,7 @@ expdir         = "fakedata_3reg"
 for varname in ("SST",):
     #varname        = "SST"               # Select which variable to use
     bbox           = [-80,0,0,65]        # Bounding box of predictor
-    leads          = np.arange(0,25,3)   # Time ahead (in years) to forecast AMV
+    leads          = (0,)#np.arange(0,25,3)   # Time ahead (in years) to forecast AMV
     thresholds     = [-1,1]#[1/3,2/3]    # Thresholds (standard deviations, or quantile values) 
     quantile       = False               # Set to True to use quantiles
     nsamples       = 300                 # Number of samples for each class. Set to None to use all
@@ -50,7 +50,7 @@ for varname in ("SST",):
 
     # Training/Testing Subsets
     percent_train  = 0.8              # Percentage of data to use for training (remaining for testing)
-    runids         = (0,) #np.arange(11,50,1) # Which runs to do
+    runids         = (0,11,1) #np.arange(11,50,1) # Which runs to do
     
     #numruns        = 10    # Number of times to train for each leadtime
     
