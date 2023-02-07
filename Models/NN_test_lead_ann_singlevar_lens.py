e928ceb8e9d3509b1ed3c74fc85677c508eb1155
Glenn Liu <glenn.y.liu@gmail.com>
2023-02-07 18:53:30 -0500

    Try quantile

--- Models/NN_test_lead_ann_singlevar_lens.py
+++ Models/NN_test_lead_ann_singlevar_lens.py
@@ -79,8 +79,8 @@ for d in range(len(dataset_names)):
         # Data preparation settings
         bbox           = [-80,0,0,65]               # Bounding box of predictor
         leads          = np.arange(0,26,1)#[a for a in np.arange(0,26,1) if a not in np.arange(0,27,3)]#np.arange(0,27,3)#(0,)     # np.arange(0,25,3)   # Time ahead (in years) to forecast AMV
-        thresholds     = [-1,1]#[1/3,2/3]           # Thresholds (standard deviations, or quantile values) 
-        quantile       = False                      # Set to True to use quantiles
+        thresholds     = [1/3,2/3]      #[-1,1]#     # Thresholds (standard deviations, or quantile values) 
+        quantile       = True                      # Set to True to use quantiles
         
         usefakedata    = None                       # Set to None, or name of fake dataset.
         region         = None                       # Set region of analysis (None for basinwide)
