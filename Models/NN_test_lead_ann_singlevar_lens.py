d795894ae7f19b32b1eae9cf9dfe1317b903c486
Glenn Liu <glenn.y.liu@gmail.com>
2023-02-14 21:19:29 -0500

    Run MIROC Quantile, limit time period

--- Models/NN_test_lead_ann_singlevar_lens.py
+++ Models/NN_test_lead_ann_singlevar_lens.py
@@ -34,7 +34,7 @@ import sys
 #%% Import some parameters (add more here eventually)
 
 cmipver = 6
-do_only = ["CESM2",]
+do_only = ["MIROC",]
 
 sys.path.append("../")
 import predict_amv_params as pparams
@@ -65,7 +65,6 @@ ens        = 40
 #%% User Edits
 # -------------
 
-
 #datasetname        = "CESM1"
 #ystart             = 1920
 yend                = 2014
@@ -90,8 +89,8 @@ for d in range(len(dataset_names)):
         # Data preparation settings
         bbox           = [-80,0,0,65]               # Bounding box of predictor
         leads          = np.arange(0,25,3)#[a for a in np.arange(0,26,1) if a not in np.arange(0,27,3)]#np.arange(0,27,3)#(0,)     # np.arange(0,25,3)   # Time ahead (in years) to forecast AMV
-        thresholds     = [-1,1]#[1/3,2/3]      #     # Thresholds (standard deviations, or quantile values) 
-        quantile       = False                      # Set to True to use quantiles
+        thresholds     = [1/3,2/3]      #  [-1,1]#   # Thresholds (standard deviations, or quantile values) 
+        quantile       = True                      # Set to True to use quantiles
         
         usefakedata    = None                       # Set to None, or name of fake dataset.
         region         = None                       # Set region of analysis (None for basinwide)
