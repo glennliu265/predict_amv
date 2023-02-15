70ee9285ace13623a71dd52b59784763ed801855
Glenn Liu <glenn.y.liu@gmail.com>
2023-02-15 12:49:27 -0500

    Retrain MIROC6, Quantile, 1920-2005

--- Models/NN_test_lead_ann_singlevar_lens.py
+++ Models/NN_test_lead_ann_singlevar_lens.py
@@ -34,7 +34,7 @@ import sys
 #%% Import some parameters (add more here eventually)
 
 cmipver = 6
-do_only = ["CESM2",]
+do_only = ["MIROC6",]
 
 sys.path.append("../")
 import predict_amv_params as pparams
