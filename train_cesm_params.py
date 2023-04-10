f9e79d16d3aa67e3f981b1f5ea20b2ae94446d75
Glenn Liu <glenn.y.liu@gmail.com>
2023-04-10 14:08:40 -0400

    Added training experiment parameters

--- train_cesm_params.py
+++ train_cesm_params.py
@@ -322,8 +322,27 @@ expdict['shuffle_trainsplit']   = False             # Set to False to maintain s
 
 train_params_all[expname]       = expdict.copy()
 
-
 #%%
+"""
+FNN4_128_SingleVar_debug1_shuffle_trainsplit
+
+Try rerunning the script above, but with shuffled data.
+"""
+
+# # Create Experiment Directory (note that expname = expdir in the original script)
+expname                         = "FNN4_128_SingleVar_debug1_shuffle_trainsplit"
+
+# Copy dictionary from above
+expdict                         = train_params_all["FNN4_128_SingleVar_Rerun100"].copy()
+
+# Cross Validation Options
+expdict['shuffle_class']        = False            # Set to True to sample DIFFERENT subsets prior to class subsetting
+expdict['shuffle_trainsplit']   = True             # Set to False to maintain same set for train/test/val split
+
+
+# Runids 
+train_params_all[expname]       = expdict.copy()
+
 
 """
 
