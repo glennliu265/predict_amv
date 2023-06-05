d4a813b90de65fbb755bf1284b5f88977bc860d3
Glenn Liu <glenn.y.liu@gmail.com>
2023-06-05 14:31:15 -0400

    Rewrote Regridding Script
    - match loading structure of make_landice_mask
    - DO NOT flip to lon180, instead use reference variable (LANDFRAC) to grab lat/lon
    - Save output separately in CESM_data
    - Does not currently support PIC

--- Preprocessing/regrid_ocean_variable.py
+++ Preprocessing/regrid_ocean_variable.py
@@ -2,257 +2,167 @@
 # -*- coding: utf-8 -*-
 """
 
-Regrid MLD (or selected variable) to cartesian lat/lon grid
-and prepare seasonal means
+Regrid Ocean Variable
 
-Uses xesmf environment. Files are large--need to figure out compression
-or datatype change.
+Regrids CESM1-LENS ocean output from tripolar to cartesian lat/lon grid.
 
-Created on Tue Feb  2 21:24:45 2021
+Saves output to                 : CESM_data/CESM1_Ocean_Regridded/[varname]/
+    Filename structure is (ex.) : SSS_HTR_bilinear_regridded_ens01.nc
 
+Uses xesmf environment. 
+
+Created on Mon Jun 5 13:23 2023 
 @author: gliu
 """
 
 import time
 import numpy as np
 import xarray as xr
-import glob
-from scipy.io import loadmat
 from tqdm import tqdm
 import xesmf as xe
+import sys
 
+# -------------
 #%% User Edits
+# -------------
 
-varname       = "BSF" # "HMXL"
-mconfig       = "FULL_HTR" # [FULL_PIC, SLAB_PIC, FULL_HTR]
+# Indicate Machine
+machine       = "stormtrack"
 
-use_xesmf     = True # Use xESMF for regridding. False = box average
-method        = "bilinear" # regridding method
+# Indicate the Variable and Scenario
+vnames        = ["SSS","SSH",] # "HMXL"
+mconfig       = "HTR" # 'HTR' or 'RCP85'
 
-use_mfdataset = False # Open all datasets at once (currently only works for PIC, boxavg)
-outpath       = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/%s/" % varname
+# Indicate Regridding Options
+use_xesmf     = True        # Use xESMF for regridding. False = box average
+use_mfdataset = False       # Open all datasets at once (currently only works for PIC, boxavg)
+method        = "bilinear"  # regridding method
+reference_var = "LANDFRAC"  # Reference variable from CAM output
 
-# Set ncsearch string on stormtrack based on input dataset
-catdim  = 'time'
-savesep = False # Save all files together
-if mconfig == "FULL_PIC":
-    ncsearch      = "b.e11.B1850C5CN.f09_g16.*.pop.h.%s.*.nc" % varname
-elif mconfig == "SLAB_PIC":
-    ncsearch      = "e.e11.B1850C5CN.f09_g16.*.pop.h.%s.*.nc" % varname
-elif mconfig == "FULL_HTR":
-    ncsearch      = "b.e11.B20TRC5CNBDRD.f09_g16.*.pop.h.%s.*.nc" % varname
-    catdim  = 'ensemble'
-    savesep = True # Save each ensemble member separately
-    use_mfdataset = False
+# Indicate paths
+outpath       = "../../CESM_data/CESM1_Ocean_Regridded/"
+datpath       = None 
 
-# Adjust data path on stormtrack
-if varname == "SSS":
-    datpath   = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/processed/ocn/proc/tseries/monthly/SSS/"
-else:
-    datpath   = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/downloaded/ocn/proc/tseries/monthly/%s/" % varname
+# if machine == "stormtrack": # Adjust data path on stormtrack (hack fix...)
+#     if varname == "SSS":
+#         datpath   = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/processed/ocn/proc/tseries/monthly/SSS/"
+#     else:
+#         datpath   = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/downloaded/ocn/proc/tseries/monthly/%s/" % varname
 
-# Set up variables to keep for preprocessing script
-varkeep   = [varname,"TLONG","TLAT","time"]
+# ----------
+#%% Import Packages + Paths based on machine
+# ----------
+# Copied from make_landice_mask.py
 
-#%% Functions
+# Get Project parameters
+sys.path.append("../")
+import predict_amv_params as pparams
+
+# Get paths based on machine
+machine_paths = pparams.machine_paths[machine]
+
+# Import custom modules
+sys.path.append(machine_paths['amv_path'])
+from amv import loaders,proc
+
+# Get data path for raw CESM1 output
+if datpath is None:
+    datpath     = machine_paths['datpath_raw_ocn']
+datpath_atm = machine_paths['datpath_raw_atm']
+    
+
+# Get experiment bounding box for preprocessing
+bbox_crop    = pparams.bbox_crop
+
+# ----------
+#%% Set some additional settings based on user input
+# ----------
+st = time.time()
+
+# Set Ensemble Member Names and Restrict to the common period
+if mconfig == "RCP85":
+    mnum    = np.concatenate([np.arange(1,36),np.arange(101,106)])
+    ntime = 1140
+elif mconfig == "HTR":
+    mnum    = np.concatenate([np.arange(1,36),np.arange(101,108)])
+    ntime = 1032
+nens = len(mnum)
+    
+# ---------
+#%% Set some functions
+# ---------
 
 # Define preprocessing variable
-def preprocess(ds,varlist=varkeep):
+def preprocess(ds,keepvars=None):
     """"preprocess dataarray [ds],dropping variables not in [varlist] and 
     selecting surface variables at [lev=-1]"""
     # Drop unwanted dimension
-    dsvars = list(ds.variables)
-    remvar = [i for i in dsvars if i not in varlist]
-    ds = ds.drop(remvar)
+    if keepvars is not None:
+        ds = proc.ds_dropvars(ds,keepvars=keepvars)
     
     # # Correct first month (Note this isn't working)
-    if ds.time.values[0].month != 1:
-         startyr = "%04i-01-01" % ds.time.values[0].year
-         endyr = "%04i-12-01" % (ds.time.values[-1].year-1)
-         correctedtime = xr.cftime_range(start=startyr,end=endyr,freq="MS",calendar="noleap") 
-         ds = ds.assign_coords(time=correctedtime) 
-         print("Corrected time to be from %s to %s"% (startyr,endyr))
+    ds = proc.fix_febstart(ds)
     return ds
 
+# ---------------------------------
+#%% Load Lat/Lon Universal Variable
+# ---------------------------------
 
-def getpt_pop_array(lonf,latf,invar,tlon,tlat,searchdeg=0.75,printfind=True,verbose=False):
+# Set up reference lat/lon
+ds_atm = loaders.load_htr(reference_var,1,datpath=datpath_atm)
+ds_atm = ds_atm.isel(time=0)
+lon    = ds_atm.lon.values
+lat    = ds_atm.lat.values
+
+ds_out = xr.Dataset({'lat':lat,'lon':lon})
+
+# ---------------------------------
+#%% Load the land and ice fractions
+# ---------------------------------
+nvars    = len(vnames)
+
+for v in range(nvars):
     
-    """
-    IMPT: assumes input variable is of the shape [lat x lon x otherdims]
-    tlon = ARRAY [lat x lon]
-    tlat = ARRAY [lat x lon]
-    """
+    varname = vnames[v]
     
-    if lonf < 0:# Convet longitude to degrees East
-        lonf += 360
+    # Adjust data path on stormtrack machine (hack fix...)
+    if machine == "stormtrack": 
+        if varname == "SSS":
+            datpath   = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/processed/ocn/proc/tseries/monthly/"
+        else:
+            datpath   = "/vortex/jetstream/climate/data1/yokwon/CESM1_LE/downloaded/ocn/proc/tseries/monthly/" 
     
-    # Query Points
-    quer = np.where((lonf-searchdeg < tlon) & (tlon < lonf+searchdeg) & (latf-searchdeg < tlat) & (tlat < latf+searchdeg))
-    latid,lonid = quer
+    # Set preprocessing function (don't need this since I load to dataarray)
+    #preprocess_var = lambda x : preprocess(x,keepvars=["TLONG","TLAT","time",varname])
     
-    if printfind:
-        print("Closest LAT to %.1f was %s" % (latf,tlat[quer]))
-        print("Closest LON to %.1f was %s" % (lonf,tlon[quer]))
+    # Loop for ensemble members
+    for e in tqdm(range(nens)):
         
-    if (len(latid)==0) | (len(lonid)==0):
-        if verbose:
-            print("Returning NaN because no points were found for LAT%.1f LON%.1f"%(latf,lonf))
-        return np.nan
+        # Load the dataarray
+        N = mnum[e]
+        ds = loaders.load_htr(varname,N,datpath=datpath,atm=False)
+        ds = preprocess(ds)
         
-    # Locate points on variable
-    if invar.shape[:2] != tlon.shape:
-        print("Warning, dimensions do not line up. Make sure invar is Lat x Lon x Otherdims")
-        
-    return invar[latid,lonid,:].mean(0) # Take mean along first dimension
-
-
-#%% Load in data
-
-if not use_xesmf:
-    method = "boxAVG"
-
-savename = "%s%s_%s_%s.nc" % (outpath,varname,mconfig,method)
-
-# Get file names
-globby = datpath+ncsearch
-nclist =glob.glob(globby)
-nclist = [nc for nc in nclist if "OIC" not in nc]
-nclist.sort()
-print("Found %i items" % (len(nclist)))
-
-# Set up target latitude and longitude
-latlonmat = "/home/glliu/01_Data/CESM1_LATLON.mat"
-ll   = loadmat(latlonmat)
-lat  = ll['LAT'].squeeze()
-lon  = ll["LON"].squeeze()
-lon1 = np.hstack([lon[lon>=180]-360,lon[lon<180]])
-#lon1,_ = proc.lon360to180(lon,np.zeros((288,192,1)))
-
-# Read in variables (all at once)
-if use_mfdataset or not use_xesmf:
-    ds = xr.open_mfdataset(nclist,concat_dim='time',
-                       preprocess=preprocess,
-                       combine='nested',
-                       parallel="True",
-                      )
-
-# Define new grid (note: seems to support flipping longitude)
-ds_out = xr.Dataset({'lat':lat,
-                     'lon':lon1})
-if use_xesmf:
-
-    start = time.time()
-
-    # Get Data Array, and rename coordinates to lon/lat
-    if ~use_mfdataset:
-        
-        ds_rgrd = [] # Loop thru each array (mfdataset doesn't seem to be working?)
-        for nc in tqdm(range(len(nclist))):
-            ds = xr.open_dataset(nclist[nc])
-            
-            ds = ds.rename({"TLONG": "lon", "TLAT": "lat"})
-            da = ds
-            #da = ds[varname] # Using dataarray seems to throw an error
-            
-            # Initialize Regridder
-            regridder = xe.Regridder(da,ds_out,method,periodic=True)
-            #print(regridder)
-                        
-            # Regrid
-            daproc = regridder(da[varname]) # Need to input dataarray
-            
-            print("Finished regridding in %f seconds" % (time.time()-start))
-            ds_rgrd.append(daproc)
-            
-            # Save each ensemble member separately (or time period)
-            if savesep: 
-                savename = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/%s_%s_%s_num%02i.nc" % (varname,mconfig,method,nc)
-                daproc.to_netcdf(savename,
-                                 encoding={varname: {'zlib': True}})
-                
-        # Concatenate along selected dimension
-        if not savesep:
-            dsproc = xr.concat(ds_rgrd,dim=catdim)
-        
-    else: # Do all at once (seems to be failing due to cf compliant issue)
-        
+        # Rename Latitude/Longitude to prepare for regridding
         ds = ds.rename({"TLONG": "lon", "TLAT": "lat"})
-        #ds = ds.rename_dims({"nlon": "longitude", "nlat": "latitude"})
-        da = ds#[varname]
+        ds = ds.to_dataset()
         
-        # Define new grid (note: seems to support flipping longitude)
-        ds_out = xr.Dataset({'lat':lat,
-                             'lon':lon1})
-    
         # Initialize Regridder
-        regridder = xe.Regridder(da,ds_out,method,periodic=True)
-        print(regridder)
-    
-    
+        regridder = xe.Regridder(ds,ds_out,method,periodic=True)
+        
         # Regrid
-        dsproc = regridder(da[varname])
-        print("Finished regridding in %f seconds" % (time.time()-start))  
-    
-    # Save the data
-    if not savesep:
-        dsproc.to_netcdf(savename,
+        daproc = regridder(ds) # Need to input dataarray
+        
+        # Save
+        savename = "%s%s/%s_%s_%s_regridded_ens%02i.nc" % (outpath,varname,varname,
+                                                  mconfig,method,e+1)
+        daproc.to_netcdf(savename,
                          encoding={varname: {'zlib': True}})
-    
-else:
+print("Regridded all ocean variables in %.2fs" % (time.time()-st))
 
-    # Load variables in 
-    st    = time.time()
-    hmxl  = ds[varname].values
-    tlon  = ds.TLONG.values
-    tlat  = ds.TLAT.values
-    times = ds.time.values
-    print("Read out data in %.2fs"%(time.time()-st))
-    
 
-    # Transpose the data
-    h = hmxl.transpose(1,2,0) # [384,320,time]
-    h.shape
-    
-    # Loop time
-    
-    start = time.time()
-    icount= 0
-    stol  = 0.75
-    hclim = np.zeros((lon1.shape[0],lat.shape[0],h.shape[2]))
-    for o in tqdm(range(lon1.shape[0])):
-        
-        # Get Longitude Value
-        lonf = lon1[o]
-        
-        # Convert to degrees Easth
-        if lonf < 0:
-            lonf = lonf + 360
-        
-        for a in range(0,lat.shape[0]):
-            
-            
-            # Get latitude indices
-            latf = lat[a]
-            
-            # Get point
-            value = getpt_pop_array(lonf,latf,h,tlon,tlat,searchdeg=stol,printfind=False)
-            if np.any(np.isnan(value)):
-                msg = "Land Point @ lon %f lat %f" % (lonf,latf)
-                hclim[o,a,:] = np.ones(h.shape[2])*np.nan
-                
-            else:
-                hclim[o,a,:] = value.copy()
-            icount +=1
-            #print("Completed %i of %i" % (icount,lon1.shape[0]*lat.shape[0]))
-            
-            
-    print("Finished in %f seconds" % (time.time()-start))  
-    
-    dsproc = xr.DataArray(hclim,
-                      dims={'lon':lon1,'lat':lat,'time':times},
-                      coords={'lon':lon1,'lat':lat,'time':times},
-                      name = varname
-                      )
-    dsproc.to_netcdf("/stormtrack/data3/glliu/01_Data/02_AMV_Project/02_stochmod/%s_PIC.nc" % (varname),
-                     encoding={varname: {'zlib': True}})
+
+
+
+
 
