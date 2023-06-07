daba3f2479fe55a8c6700fd1b9ce627c48b23747
Glenn Liu <glenn.y.liu@gmail.com>
2023-06-06 23:05:24 -0400

    Update Target Calculation Script
    - Update documentation
    - Load bounding boxes from params file
    - Set new output input paths
    - Remove deseason, normalizing, regridding, etc
    - Add application of new land/ice mask
    - Change output filename amv --> NASST

--- Preprocessing/prepare_regional_targets.py
+++ Preprocessing/prepare_regional_targets.py
@@ -5,7 +5,19 @@
 Copied from prepare_training_validation.py
 Essentially only does the SST component and averages over particular regions
 
+
+
 Created on Fri Dec  2 11:48:40 2022
+
+Update: 2023.06.06 -> Works with new output from prep_data_byvariable
+
+   - Loads in SST preprocessed by [prep_data_byvariable] in ../../CESM_data/Predictors/
+         Input naming style  : CESM1LE_SST_NAtl_19200101_20051201_bilinear_detrend0_regridNone.nc
+   - Outputs Indices averaged over each region enumerated from [predict_amv_params]
+     Files are located in ../../CESM_data/Targets/
+         
+         Output naming style : CESM1LE_label_NAT_NASST_index_detrend0_regridNone.npy
+     
 
 @author: gliu
 """
@@ -13,81 +25,73 @@ Created on Fri Dec  2 11:48:40 2022
 import numpy as np
 import xarray as xr
 import xesmf as xe
+import sys
 
+# ------------
+#%% User Edits
+# ------------
+
+machine = "stormtrack"
 detrend = False # Detrending is currently not applied
 regrid  = None # Set to desired resolution. Set None for no regridding.
 
+datpath  = "../../CESM_data/Predictors/" # Path to SST data processed by prep_data_byvariable.py
+maskpath = "../../CESM_data/Masks/"
+outpath  = "../../CESM_data/Targets/"
 # -----------------------------------------------------------------------------
-# Copied from sm_paper_stylesheet.py on 2022.12.02
-# removed NNAT
-bbox_SP     = [-60,-15,40,65]
-bbox_ST     = [-80,-10,20,40]
-bbox_TR     = [-75,-15,10,20]
-bbox_NA     = [-80,0 ,0,65]
-bbox_NA_new = [-80,0,10,65]
-bbox_ST_w   = [-80,-40,20,40]
-bbox_ST_e   = [-40,-10,20,40]
-regions     = ("SPG","STG","TRO","NAT","STGe","STGw")        # Region Names
-bboxes      = (bbox_SP,bbox_ST,bbox_TR,bbox_NA,bbox_ST_e,bbox_ST_w) # Bounding Boxes
-regionlong  = ("Subpolar","Subtropical","Tropical","North Atlantic","Subtropical (East)","Subtropical (West)",)
-
+#%% Import Packages + Paths based on machine
 # -----------------------------------------------------------------------------
 
-# --------------------------------
-# Select Box in the North Atlantic
-# --------------------------------
-sst_ds = xr.open_dataset('../../CESM_data/CESM1LE_sst_NAtl_19200101_20051201.nc')['sst'][:,:,:,:]
+# Get Project parameters
+sys.path.append("../")
+import predict_amv_params as pparams
+import amv_dataloader as dl
 
-# ----------------------------------------------------------------
-# Calculate Monhtly Anomalies (Remove mean seasonal cycle)
-# ----------------------------------------------------------------
-print('begin deseason')
-sst_deseason = (sst_ds.groupby('time.month') - sst_ds.groupby('time.month').mean('time')).groupby('time.year').mean('time')
+# Get paths based on machine
+machine_paths = pparams.machine_paths[machine]
+
+# Import custom modules
+sys.path.append(machine_paths['amv_path'])
+from amv import loaders,proc
 
 
-# --------------------------------
-# Detrend the data if option is set
-# --------------------------------
-if detrend:
-    sst_deseason = sst_deseason - sst_deseason.mean('ensemble')
-
-# -------------------------
-# Normalize and standardize
-# -------------------------
-sst_normalized = (sst_deseason - sst_deseason.mean())/sst_deseason.std()
-means  = (sst_deseason.mean(),)
-stdevs = (sst_deseason.std(),)
-
-# -------------------------
-# Regrid Variables
-# -------------------------
-if regrid is not None:
-    print("Data will be regridded to %i degree resolution"%regrid)
-    # Prepare Latitude/Longitude
-    lat = sst_ds.lat
-    lon = sst_ds.lon
-    lat_out = np.linspace(lat[0],lat[-1],regrid)
-    lon_out = np.linspace(lon[0],lon[-1],regrid)
+# Get experiment bounding box for preprocessing
+bbox_crop   = pparams.bbox_crop
+bbox_SP     = pparams.bbox_SP#[-60,-15,40,65]
+bbox_ST     = pparams.bbox_ST#[-80,-10,20,40]
+bbox_TR     = pparams.bbox_TR#[-75,-15,10,20]
+bbox_NA     = pparams.bbox_NA#[-80,0 ,0,65]
+regions     = pparams.regions
+bboxes      = (bbox_SP,bbox_ST,bbox_TR,bbox_NA,) # Bounding Boxes
+   # regionlong  = pparams.regionlong
+    #regionlong  = ("Subpolar","Subtropical","Tropical","North Atlantic","Subtropical (East)","Subtropical (West)",)
     
-    # Make Regridder
-    ds_out    = xr.Dataset({'lat': (['lat'], lat_out), 'lon': (['lon'], lon_out) })
-    regridder = xe.Regridder(sst_ds, ds_out, 'nearest_s2d')
+    
+#
+#%%
 
-    # Regrid
-    sst_out = regridder( sst_normalized.transpose('ensemble','year','lat','lon') )
-else:
-    print("Data will not be regridded")
-    sst_out = sst_normalized.transpose('ensemble','year','lat','lon') 
+# --------------------------------
+# Load the DataArray
+# --------------------------------
+ncname = "%sCESM1LE_SST_NAtl_19200101_20051201_bilinear_detrend%i_regrid%s.nc" % (datpath,detrend,regrid)
+sst_ds = xr.open_dataset(ncname)['SST'][:,:,:,:] # [Ensemble x Year x Lat x Lon]
+
+
+# ----------------------------
+# Load land/ice mask and apply
+# ----------------------------
+limask = dl.load_limask(datpath=maskpath,bbox=bbox_crop) #[lat x lon]
+sst_ds = sst_ds * limask[None,None,...]
 
 # -----------------------------------------------
 # Calculate the AMV Index (Area weighted average)
 # -----------------------------------------------
 for b,bb in enumerate(bboxes):
-    sst_out_reg = sst_out.sel(lon=slice(bb[0],bb[1]),lat=slice(bb[2],bb[3]))
+    sst_out_reg = sst_ds.sel(lon=slice(bb[0],bb[1]),lat=slice(bb[2],bb[3]))
     amv_index   = (np.cos(np.pi*sst_out_reg.lat/180) * sst_out_reg).mean(dim=('lat','lon'))
-    savename = 'CESM_label_%s_amv_index_detrend%i_regrid%s.npy' % (regions[b],detrend,regrid)
+    savename = '%sCESM1LE_label_%s_NASST_index_detrend%i_regrid%s.npy' % (outpath,regions[b],detrend,regrid)
     np.save(savename,amv_index[:,:])
-    print("Saved Region %s ([%s]) as %s" % (regionlong[b],bb,savename))
+    print("Saved Region %s ([%s]) as %s" % (regions[b],bb,savename))
 
 
 
